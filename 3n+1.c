9217c27d5636bb13805dbfbd0b5f120e0f38c062
D2025 <108764766+D2025@users.noreply.github.com>
2022-07-26 13:08:02 +0200

    Code for this algorithm

---
+++ 3n+1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void) {
+  long long int n = 172800000001;
+  long long int i = n;
+  while(1==1){
+    while(i!=1){
+      //main calculations
+      if(i%2 == 0){
+        i = i/2;
+      }
+      
+      else{
+        i = i*3+1;
+      }
+      //end
+
+      //loop detection
+      if(i==n){
+        printf("Number %lld looped", n);
+        break;
+      }
+      //end
+      
+      //optimization
+      if(i<n){
+        i = 1;
+      }
+      //end
+    }
+  if(n%100000000==1){
+    printf("%lld reached 1\n", n);
+  }
+  n += 2;
+  i = n;
+  }
+}
